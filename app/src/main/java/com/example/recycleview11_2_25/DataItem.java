063748e30eb8ca4878c77165b538f2ee91134f94
noamAvni2000 <nmnby7399@gmail.com>
2025-02-11 14:37:32 +0200

    second commit MyAdpater

--- app/src/main/java/com/example/recycleview11_2_25/DataItem.java
+++ app/src/main/java/com/example/recycleview11_2_25/DataItem.java
@@ -1,4 +1,19 @@
 package com.example.recycleview11_2_25;
 
 public class DataItem {
+    private String s;
+
+    public DataItem(String s) {
+        this.s = s;
+    }
+
+    public String getS() {
+        return s;
+    }
+
+    public void setS(String s) {
+        this.s = s;
+    }
+
+
 }
---
+++ app/src/main/java/com/example/recycleview11_2_25/MyAdapter.java
@@ -0,0 +1,56 @@
+package com.example.recycleview11_2_25;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>  {
+    private DataItem [] mData;
+    private Context mContext;
+    public MyAdapter(Context context, DataItem[]data  ) {
+        this.mContext=context;
+        this.mData=data;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v=LayoutInflater.from(mContext).inflate(R.layout.list_item, parent, false);
+        return new MyViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.myEt.setText(" ");
+        holder.myBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = holder.myEt.getText().toString();
+                Toast.makeText(v.getContext(), text, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        EditText myEt;
+        Button myBtn;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            myEt = itemView.findViewById(R.id.editText_item);
+            myBtn= itemView.findViewById(R.id.button_submit);
+        }
+    }
+}
